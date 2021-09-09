aa21f1c1dd52091bfa963daac9f8847c02e64a5b
David dos Santos Melo <david.melo10@hotmail.com>
2021-09-09 20:28:53 -0300

    Codigo

---
+++ PUC.IOT.BATTERY/PUC.IOT.BATTERY/PUC.IOT.BATTERY/Battery.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Plugin.Battery.Abstractions;
+using System;
+
+namespace PUC.IOT.BATTERY
+{
+    [Serializable]
+    public  class Battery
+    {
+       public string dispositivo { get; set; }
+        public int percentual { get; set; }
+        [JsonIgnore]
+        public BatteryStatus BatteryStatus { get; set; }
+        public DateTime data { get; set; }
+
+
+        public string status
+        {
+            get
+            {
+                string status = "";
+                switch (BatteryStatus)
+                {
+                    case BatteryStatus.Charging:
+                        status = "Carregando";
+                        break;
+                    case BatteryStatus.Discharging:
+                        status = "Descarregando";
+                        break;
+                    case BatteryStatus.Full:
+                        status = "Cheia";
+                        break;
+                    case BatteryStatus.NotCharging:
+                        status = "Não está carregando";
+                        break;
+                    case BatteryStatus.Unknown:
+                        status = "Desconhecido";
+                        break;
+                }
+
+                return status;
+            }
+        }
+    }
+}
---
+++ PUC.IOT.BATTERY/PUC.IOT.BATTERY/PUC.IOT.BATTERY/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PUC.IOT.BATTERY
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private const int DelayDefault = 1000;
+        private int Delay = 0;
+        private BatteryService batteryService;
+        private Battery battery;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadParameters();
+            CheckStatus();
+        }
+
+        private void LoadParameters()
+        {
+            batteryService = new BatteryService();
+            battery = new Battery();
+
+            if (!int.TryParse(AppSettingsManager.Settings["Delay"], out Delay))
+                Delay = DelayDefault;
+        }
+
+        private void CheckStatus()
+        {
+
+            battery = batteryService.UpdateStatus();
+
+            UpdateInfo();
+
+            CheckStatusTaskDelay();
+        }
+
+
+        private void UpdateInfo()
+        {
+            lblDeviceName.Text = $"Dispositivo : {battery.dispositivo}";
+            lblChargeLevel.Text = $"Bateria : {battery.percentual.ToString()}";
+            lblStatus.Text = $"Status : {battery.status}";
+            lblLastUpdate.Text = $"Última Atualização : {battery.data.ToString("dd/MM/yyyy HH:mm:ss")}";
+        }
+
+        async Task CheckStatusTaskDelay()
+        {
+            await Task.Delay(Delay);
+            CheckStatus();
+        }
+
+    }
+}
+
